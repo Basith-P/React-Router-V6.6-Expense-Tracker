9bbdfdae9b01dfa01183ae4d5f3571fda9ab977b
Basith P <basithpofficial@gmail.com>
2023-04-14 16:38:26 +0530

    add: add expense form

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,7 @@
 export const wait = () =>
   new Promise((res) => setTimeout(res, Math.random() * 2000));
 
+// colors
 const generateRandomColor = () => {
   const existingBudgetLength = fetchData("budgets")?.length ?? 0;
   return `${existingBudgetLength * 34} 65% 50%`;
@@ -11,21 +12,39 @@ export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
 };
 
-export const createBudget = (name, amount) => {
+// create budget
+export const createBudget = ({ name, amount }) => {
   const newItem = {
     id: crypto.randomUUID(),
     name: name,
-    amount: +amount,
     createdAt: Date.now(),
+    amount: +amount,
     color: generateRandomColor(),
-    // color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
   };
-
-  const data = fetchData("budgets") ?? [];
-  data.push(newItem);
-  localStorage.setItem("budgets", JSON.stringify(data));
+  const existingBudgets = fetchData("budgets") ?? [];
+  return localStorage.setItem(
+    "budgets",
+    JSON.stringify([...existingBudgets, newItem])
+  );
 };
 
+// create expense
+export const createExpense = ({ name, amount, budgetId }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    budgetId: budgetId,
+  };
+  const existingExpenses = fetchData("expenses") ?? [];
+  return localStorage.setItem(
+    "expenses",
+    JSON.stringify([...existingExpenses, newItem])
+  );
+};
+
+// delete item
 export const deleteItem = ({ key }) => {
-  localStorage.removeItem(key);
+  return localStorage.removeItem(key);
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,9 +7,10 @@ import { toast } from "react-toastify";
 // components
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
+import AddExpenseForm from "../components/AddExpenseForm";
 
 //  helper functions
-import { createBudget, fetchData, wait } from "../helpers";
+import { createBudget, createExpense, fetchData, wait } from "../helpers";
 
 // loader
 export function dashboardLoader() {
@@ -21,12 +22,15 @@ export function dashboardLoader() {
 // action
 export async function dashboardAction({ request }) {
   await wait();
+
   const data = await request.formData();
   const { _action, ...values } = Object.fromEntries(data);
+
+  // new user submission
   if (_action === "newUser") {
     try {
-      localStorage.setItem("userName", JSON.stringify(formData.userName));
-      return toast.success(`Welcome, ${formData.userName}`);
+      localStorage.setItem("userName", JSON.stringify(values.userName));
+      return toast.success(`Welcome, ${values.userName}`);
     } catch (e) {
       throw new Error("There was a problem creating your account.");
     }
@@ -43,6 +47,19 @@ export async function dashboardAction({ request }) {
       throw new Error("There was a problem creating your budget.");
     }
   }
+
+  if (_action === "createExpense") {
+    try {
+      createExpense({
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget,
+      });
+      return toast.success(`Expense ${values.newExpense} created!`);
+    } catch (e) {
+      throw new Error("There was a problem creating your expense.");
+    }
+  }
 }
 
 const Dashboard = () => {
@@ -53,15 +70,23 @@ const Dashboard = () => {
       {userName ? (
         <div className="dashboard">
           <h1>
-            Welcome, <span className="accent">{userName}</span>
+            Welcome back, <span className="accent">{userName}</span>
           </h1>
           <div className="grid-sm">
-            {/* {budgets ? () : ()} */}
-            <div className="grid-lg">
-              <div className="flex-lg">
+            {budgets && budgets.length > 0 ? (
+              <div className="grid-lg">
+                <div className="flex-lg">
+                  <AddBudgetForm />
+                  <AddExpenseForm budgets={budgets} />
+                </div>
+              </div>
+            ) : (
+              <div className="grid-sm">
+                <p>Personal budgeting is the secret to financial freedom.</p>
+                <p>Create a budget to get started!</p>
                 <AddBudgetForm />
               </div>
-            </div>
+            )}
           </div>
         </div>
       ) : (
